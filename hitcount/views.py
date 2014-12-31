d752a80932779d6e362714680567a0797d3d9286
mohamed-ali-affes <mohamed.ali.affes@gmail.com>
2014-12-31 11:57:49 +0100

    Fixing simplejson problem for Python 2.6+

--- hitcount/views.py
+++ hitcount/views.py
@@ -1,10 +1,11 @@
+import json as simplejson
 from django.http import Http404, HttpResponse, HttpResponseBadRequest
-from django.utils import simplejson
+# from django.utils import simplejson
 from django.conf import settings
-from django.contrib.contenttypes.models import ContentType
 
 from hitcount.utils import get_ip
 from hitcount.models import Hit, HitCount, BlacklistIP, BlacklistUserAgent
+
 
 def _update_hit_count(request, hitcount):
     '''
@@ -25,8 +26,7 @@ def _update_hit_count(request, hitcount):
     ip = get_ip(request)
     user_agent = request.META.get('HTTP_USER_AGENT', '')[:255]
     hits_per_ip_limit = getattr(settings, 'HITCOUNT_HITS_PER_IP_LIMIT', 0)
-    exclude_user_group = getattr(settings,
-                            'HITCOUNT_EXCLUDE_USER_GROUP', None)
+    exclude_user_group = getattr(settings, 'HITCOUNT_EXCLUDE_USER_GROUP', None)
 
     # first, check our request against the blacklists before continuing
     if BlacklistIP.objects.filter(ip__exact=ip) or \
@@ -47,21 +47,22 @@ def _update_hit_count(request, hitcount):
             return False
 
     # create a generic Hit object with request data
-    hit = Hit(  session=session_key,
-                hitcount=hitcount,
-                ip=get_ip(request),
-                user_agent=request.META.get('HTTP_USER_AGENT', '')[:255],)
+    hit = Hit(
+        session=session_key,
+        hitcount=hitcount,
+        ip=get_ip(request),
+        user_agent=request.META.get('HTTP_USER_AGENT', '')[:255],)
 
     # first, use a user's authentication to see if they made an earlier hit
     if user.is_authenticated():
-        if not qs.filter(user=user,hitcount=hitcount):
-            hit.user = user #associate this hit with a user
+        if not qs.filter(user=user, hitcount=hitcount):
+            hit.user = user  # associate this hit with a user
             hit.save()
             return True
 
     # if not authenticated, see if we have a repeat session
     else:
-        if not qs.filter(session=session_key,hitcount=hitcount):
+        if not qs.filter(session=session_key, hitcount=hitcount):
             hit.save()
 
             # forces a save on this anonymous users session
@@ -70,6 +71,7 @@ def _update_hit_count(request, hitcount):
             return True
 
     return False
+
 
 def json_error_response(error_message):
     return HttpResponse(simplejson.dumps(dict(success=False,
@@ -80,15 +82,14 @@ def json_error_response(error_message):
 # hit count so we could update it via javascript (since each view will
 # be one behind).
 def update_hit_count_ajax(request):
-    '''
+    """
     Ajax call that can be used to update a hit count.
 
     Ajax is not the only way to do this, but probably will cut down on
     bots and spiders.
 
     See template tags for how to implement.
-    '''
-
+    """
     # make sure this is an ajax request
     if not request.is_ajax():
         raise Http404()
@@ -111,4 +112,4 @@ def update_hit_count_ajax(request):
         status = "no hit recorded"
 
     json = simplejson.dumps({'status': status})
-    return HttpResponse(json,mimetype="application/json")
+    return HttpResponse(json, mimetype="application/json")
